468e7b4b0818f194d2143e80d9f5c48440453bb0
jctiru <jonathanchristiantiru@gmail.com>
2019-10-09 18:58:26 +0800

    Change generation type to table and add generator configuration

--- hibernate-sandbox/src/test/hibernatesandbox/inheritance/tableperclass/entity/Payment.java
+++ hibernate-sandbox/src/test/hibernatesandbox/inheritance/tableperclass/entity/Payment.java
@@ -8,12 +8,33 @@ import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+/**
+ * 
+ * References
+ * 
+ * Hibernate 5.2
+ * https://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html
+ *
+ * DefaultIdentifierGeneratorFactory for generator strategy
+ * https://github.com/hibernate/hibernate-orm/blob/master/hibernate-core/src/main/java/org/hibernate/id/factory/internal/DefaultIdentifierGeneratorFactory.java
+ * 
+ * TableGenerator (enhanced-table)
+ * https://github.com/hibernate/hibernate-orm/blob/master/hibernate-core/src/main/java/org/hibernate/id/enhanced/TableGenerator.java
+ * 
+ */
+
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Payment {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GenericGenerator(name = "table", strategy = "enhanced-table", parameters = {
+			@Parameter(name = "table_name", value = "hibernate_sequences"),
+			@Parameter(name = "prefer_entity_table_as_segment_value", value = "true") })
+	@GeneratedValue(generator = "table", strategy = GenerationType.TABLE)
 	@Column(name = "id")
 	private int id;
 
