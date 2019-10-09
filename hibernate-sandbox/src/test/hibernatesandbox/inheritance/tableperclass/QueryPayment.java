52a4f55ff8f41a9897d3725b80867e1f3907fac7
jctiru <jonathanchristiantiru@gmail.com>
2019-10-09 19:14:08 +0800

    Add query payment example for table per class strategy

---
+++ hibernate-sandbox/src/test/hibernatesandbox/inheritance/tableperclass/QueryPayment.java
@@ -0,0 +1,43 @@
+package test.hibernatesandbox.inheritance.tableperclass;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import test.hibernatesandbox.inheritance.tableperclass.entity.Check;
+import test.hibernatesandbox.inheritance.tableperclass.entity.CreditCard;
+import test.hibernatesandbox.inheritance.tableperclass.entity.Payment;
+
+public class QueryPayment {
+
+	public static void main(String[] args) {
+		try (SessionFactory factory = new Configuration()
+				.configure("hibernate_jpa_table_per_class.cfg.xml")
+				.addAnnotatedClass(Payment.class)
+				.addAnnotatedClass(CreditCard.class)
+				.addAnnotatedClass(Check.class)
+				.buildSessionFactory();
+				Session session = factory.getCurrentSession();) {
+			session.beginTransaction();
+
+			List<Payment> paymentList = session.createQuery("from Payment", Payment.class).getResultList();
+			System.out.println("All payments:");
+			paymentList.forEach(System.out::println);
+
+			System.out.println();
+			List<CreditCard> creditCardList = session.createQuery("from CreditCard", CreditCard.class).getResultList();
+			System.out.println("All credit cards:");
+			creditCardList.forEach(System.out::println);
+
+			System.out.println();
+			List<Check> checkList = session.createQuery("from Check", Check.class).getResultList();
+			System.out.println("All checks:");
+			checkList.forEach(System.out::println);
+
+			session.getTransaction().commit();
+		}
+	}
+
+}
