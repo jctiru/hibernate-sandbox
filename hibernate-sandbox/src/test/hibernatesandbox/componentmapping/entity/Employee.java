1ea088ddd43703483181e1ee7e2053d39925c0f3
jctiru <jonathanchristiantiru@gmail.com>
2019-10-11 23:01:49 +0800

    Create entities for component mapping example

---
+++ hibernate-sandbox/src/test/hibernatesandbox/componentmapping/entity/Employee.java
@@ -0,0 +1,59 @@
+package test.hibernatesandbox.componentmapping.entity;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Embedded
+	@AttributeOverrides({
+			@AttributeOverride(name = "streetAddress", column = @Column(name = "street_address")),
+			@AttributeOverride(name = "zipCode", column = @Column(name = "zip_code"))
+	})
+	private Address address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+
+}
